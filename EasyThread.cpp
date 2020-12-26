ca760fbb644c6f555d540510e8adb9628b36b9e7
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-12-26 12:15:58 +0100

    Comments

--- EasyThread.cpp
+++ EasyThread.cpp
@@ -1,29 +1,53 @@
 #include "Arduino.h"
 #include "EasyThread.h"
 
+// Constructor declaration
 EZThread::EZThread(int threads){
+    // We create the unsigned long array that will store timers for the threads
     unsigned long schedules[threads];
+
+    // We create the int array that will store the intervals at which every thread should execute
     int intervals[threads];
+
+    // We create the function pointer array that will point to the callback for every thread.
     void (*f[threads])();
+
+    //We assign the globals defined in EasyThread.h
     _f = f;
     _schedules = schedules;
     _intervals = intervals;
     _schid = 0;
 }
 
+// newThread declaration
+// This function creates a new Thread
 void EZThread::newThread(void (*f)(), int time){
+    // We store the callback function adress to the function pointer array
     *(_f + _schid) = f;
+    
+    // We store the time interval at which the thread should execute
     *(_intervals + _schid) = time;
+
+    // We initialize the timer for this thread to 0
     *(_schedules + _schid) = (unsigned long) 0;
+
+    // We increase the number of threads
     _schid++;
 }
 
 void EZThread::schedule(){
+    // We store the current time in which we are scheduling
     unsigned long now = millis();
+
+    // For every thread
     for(int i = 0; i < _schid; i++){
-        if(now - *(_schedules + _schid) >= *(_intervals + _schid)){
-            (*(_f + _schid))();
-            *(_schedules + _schid) = now;
+        // If the scheduling time - the last time  that thread executed >= interval at it should execute
+        if(now - *(_schedules + i) >= *(_intervals + i)){
+            // We call to the callback function stored in the function pointer array
+            (*(_f + i))();
+
+            // We set the internal timer to the time we started scheduling
+            *(_schedules + i) = now;
         }
     }
 }
--- EasyThread.h
+++ EasyThread.h
@@ -5,12 +5,22 @@
 
 class EZThread {
     public:
+        // Constructor takes the number of threads
         EZThread(int threads);
+
+        // Protoype for the new Thread function
         void newThread(void (*f)(void), int time);
+
+        // Prototype for the schedule function
         void schedule();
     private:
+        // Unsigned long pointer that will point to an array containing the timers for every thread
         unsigned long *_schedules;
+        // int pointer that will point to an array containing the intervals for every thread
         int *_intervals;
+        // Function double pointer that will point to an array of function pointers that point to
+        // the function callback of every thread
         void (**_f)();
+        // Int that stores the number of threads there are
         int _schid;
 };
